b872b83f220ee84f44c5fc9a4d358236746916a9
webnur <mdnuruli291@gmail.com>
2023-11-24 18:48:30 +0600

    basic setup project

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('database connect successfully')
+    const server = app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+
+    // Handle server close event to gracefully shut down the application
+    process.on('SIGINT', () => {
+      console.log('Closing server...')
+      server.close(() => {
+        console.log('Server closed.')
+        process.exit(0)
+      })
+    })
+  } catch (error) {
+    console.log('failed to connect database', error)
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+main()
+
+// import mongoose from "mongoose";
+// import app from "./app";
+// import config from './config/index';
+
+// main().catch(err => console.error(err));
+
+// async function main() {
+//   try {
+//     await mongoose.connect(config.database_url as string, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       // Add other Mongoose connection options if needed
+//     });
+//     console.log('Database connected successfully');
+
+//     const server = app.listen(config.port, () => {
+//       console.log(`Server is running on port ${config.port}`);
+//     });
+
+//     // Handle server close event to gracefully shut down the application
+//     process.on('SIGINT', () => {
+//       console.log('Closing server...');
+//       server.close(() => {
+//         console.log('Server closed.');
+//         process.exit(0);
+//       });
+//     });
+//   } catch (error) {
+//     console.error('Failed to connect to the database', error);
+//   }
+// }
